74cb2f848235da05cf86612a85412e7efc005671
azangru <andrey.azov@gmail.com>
2014-06-13 11:26:19 +0100

    Nearly there

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,8 +18,8 @@ class UsersController < Devise::RegistrationsController
           params[:q][:profile_interested_in_age_end_gteq] = searcher_age
         end
       end
-      # @search = User.where("role NOT IN (?)", "admin").where("id NOT IN (?)", current_user.id).search(params[:q])  # the bit between User and .search is written by Andrey to exclude admins and current users from search results. I think it should be properly done by scoping, but I can't figure out how.
-      @search = User.where("role NOT IN (?)", "admin").search(params[:q])
+      @search = User.where("role NOT IN (?)", "admin").where("users.id NOT IN (?)", current_user.id).search(params[:q])  # the bit between User and .search is written by Andrey to exclude admins and current users from search results. I think it should be properly done by scoping, but I can't figure out how.
+      # @search = User.where("role NOT IN (?)", "admin").search(params[:q])
       @users = @search.result.includes(:profile)
     end
   end
